f10caca09e0da12e75e8c50168bb5564a07ff2b5
Jaehyeon <parkjh9370@naver.com>
2023-08-28 20:25:04 +0900

    FEAT: User, Video DTO, entity 정의

--- src/auth/dto/req.dto.ts
+++ src/auth/dto/req.dto.ts
@@ -2,16 +2,26 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, Matches, MaxLength } from 'class-validator';
 
 export class SignupReqDto {
-  @ApiProperty({ required: true, example: 'parkjh9370@naverc.com' })
+  @ApiProperty({
+    required: true,
+    example: 'parkjh9370@naverc.com',
+  })
   @IsEmail()
   @MaxLength(30)
   email: string;
 
-  @ApiProperty({ required: true, example: 'Password1!' })
+  @ApiProperty({
+    required: true,
+    example: 'Password1!',
+  })
   @Matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*]).{10,30}$/)
   password: string;
 
-  @ApiProperty({ required: true, example: 'Password1!' })
+  @ApiProperty({
+    required: true,
+    example: 'Password1!',
+  })
+  // 숫자, 소문자, 대문자 중 1가지가 모두 들어가야 하고 10 ~ 30 글자
   @Matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*]).{10,30}$/)
   passwordConfirm: string;
 }
